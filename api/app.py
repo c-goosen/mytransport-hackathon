117192136ef75c3125eec9c22b6d05e43ca6014b
Christo Goosen <christo.goosen@opendna.ai>
2017-03-04 14:57:30 +0200

    Added interest endpoint with basic API call to mytransport api with hardcoded values, need to add geometry input and checking distance to stops

--- api/app.py
+++ api/app.py
@@ -2,6 +2,9 @@ import falcon
 import elasticsearch
 from endpoints.interest import interest
 from endpoints.root import root
+import settings
+import requests
+import json
 app = falcon.API()
 
 root = root()
@@ -9,6 +12,11 @@ interest = interest()
 app.add_route('/interest', interest)
 #root = root()
 app.add_route('/', root)
+
+##### GLobal VARS
+ACCESS_TOKEN = ""
+#CLIENT_ID = "781b4b4a-5921-4e90-ad34-ce4bfb9912c5"
+#CLIENT_SECRET = "0lXwudYL6HdSdsQwSxQcFAxbLjh56PEls9Jiq3NGaSE="
 
 if __name__ == "__main__":
     from wsgiref import simple_server
@@ -22,4 +30,7 @@ if __name__ == "__main__":
         '127.0.0.1', 8081, app
         )
     print('[INFO] Server Ready')
+    ACCESS_TOKEN = settings.ACCESS_TOKEN
+    print ACCESS_TOKEN
+
     httpd.serve_forever()
--- api/endpoints/interest.py
+++ api/endpoints/interest.py
@@ -1,10 +1,36 @@
+import os.path, sys
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir))
+import json
 import falcon
 import uuid
-
+import settings
+import requests
 class interest(object):
+    interested = {}
+    def proximity(self,req):
+       # print settings.API_URL
+        query_params = {"point":"-33.947272,18.4783663","radius":"1000"
+                      }
+        endpoint ="api/stops"
+        headers = {"Authorization": "Bearer {}".format(settings.ACCESS_TOKEN)}
+        request = requests.get("{}/{}".format(settings.API_URL,endpoint), 
+                               params=query_params, 
+                               headers=headers)
+        print request.status_code
+        print request.text
+        print json.load(request.text)
 
     def on_get(self, req, resp):
         resp.body = '{"message":"Post request needed with GeoLocation data"}'
         resp.status = falcon.HTTP_200
     def on_post(self, req, resp):
+        print req.headers
+        post_data = json.load(req.stream)
+        print post_data
+        if not post_data["geometry"]:
+            falcon.HTTPMissingParam
+        else:
+            self.proximity(req)
         pass
+
+
